57b46a310140d52524dffd40d049a06a7c965eec
danyprescendo <danypablo08@hotmail.com>
2026-08-17 21:09:55 -0300

    feat: Cria o MissaoRepository com JPA

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -1,4 +1,5 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissaoRepository {
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long>{
 }
